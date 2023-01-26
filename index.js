c81e6f0a05e52579bb709bbdbc53e788cd8efab7
unknown <ainouzusa@gmail.com>
2023-01-26 23:48:24 +0100

    1 test fail

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const { User } = require('./db');
-
+const bcrypt = require('bcrypt');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
@@ -13,12 +13,44 @@ app.get('/', async (req, res, next) => {
     next(error)
   }
 });
-
+app.post('/register', async (req, res) => {
+  try {
+      const { username, password } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 10);
+      await User.create({
+          username,
+          password: hashedPassword
+      });
+      res.send('successfully created user bobbysmiles');
+  } catch (error) {
+      res.status(500).send(error.message);
+  }
+});
 // POST /register
 // TODO - takes req.body of {username, password} and creates a new user with the hashed password
-
+app.post('/login', async (req, res) => {
+  try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+      if (!user) {
+          return res.send('Username not found');
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+          return res.status(401).send('incorrect username or password');
+      }
+      res.send('successfully logged in user bobbysmiles');
+  } catch (error) {
+      res.status(500).send(error.message);
+  }
+});
 // POST /login
 // TODO - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
+
+
+
+
+
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
